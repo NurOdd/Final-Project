1e53a3efc2d79628de7df1aabbca3c745e46a8c6
Nuria Rodriguez <nur.rodga@gmail.com>
2022-10-21 10:47:55 +0200

    add const login

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template >
    <Welcome/>
 
+
 </template>
 <script setup>
-import Welcome from './views/Welcome.vue'
-import Home from './views/Home.vue'
+import Welcome from './views/Welcome.vue';
+import Home from './views/Home.vue';
+import {onMounted} from 'vue'; 
 
 import {createClient} from '@supabase/supabase-js'
 const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY)
+
+const registro = async() =>{
+   const result = await supabase.auth.signUp({
+   email: 'nur.rodga@gmail.com',
+   password: '1234'
+}) 
+console.log (result)}
+
+onMounted() => {
+   registro()
+}
 
 </script>
 
--- src/main.js
+++ src/main.js
@@ -6,5 +6,6 @@ import {createPinia} from 'pinia'
 
 
 const pinia = createPinia()
+
 createApp(App).use(router).use(pinia).mount('#app')
 
