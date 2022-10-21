a0536347ca2a6fdacc6c67e8c1163b9977e82720
Nuria Rodriguez <nur.rodga@gmail.com>
2022-10-21 11:49:47 +0200

    add api js

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -3,6 +3,24 @@
 </template>
 
 <script setup>
+
+import {onMounted} from 'vue'; 
+
+import {createClient} from '@supabase/supabase-js'
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY)
+
+const registro = async() =>{
+   const result = await supabase.auth.signUp({
+   email: 'nur.rodga@gmail.com',
+   password: '12345678'
+}) 
+console.log (result);
+}
+
+onMounted(() => {
+   registro()
+})
+
 </script>
 
 <style scoped>
